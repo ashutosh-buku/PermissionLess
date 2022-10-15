995254e95ed4839d219685617148f2bd3a00fd97
Ashutosh Sharma <77958573+ashutosh-buku@users.noreply.github.com>
2022-10-15 12:49:18 +0530

    Initial commit

---
+++ app/src/main/java/com/ashu/permissionless/FirstFragment.kt
@@ -0,0 +1,80 @@
+package com.ashu.permissionless
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.fragment.findNavController
+import com.ashu.permissionless.databinding.FragmentFirstBinding
+import com.google.modernstorage.photopicker.PhotoPicker
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imagePicker = registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(5)) { uris ->
+            println(uris)
+        }
+
+        val videoPicker = registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(5)) { uris ->
+            println(uris)
+        }
+
+        val multimediaPicker = registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(15)) { uris ->
+            println(uris)
+        }
+
+        val gifPicker = registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(5)) { uris ->
+            println(uris)
+        }
+
+        // For images upto max 5
+        binding.btnImage.setOnClickListener {
+            imagePicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
+        // For videos upto max 5
+        binding.btnVideo.setOnClickListener {
+            videoPicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.VideoOnly))
+        }
+
+        // For image and video, limit 15
+        binding.btnMultimedia.setOnClickListener {
+            multimediaPicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo))
+        }
+
+        // For GIFs only, limit 5
+        binding.btnGif.setOnClickListener {
+            gifPicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.SingleMimeType("image/gif")))
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
